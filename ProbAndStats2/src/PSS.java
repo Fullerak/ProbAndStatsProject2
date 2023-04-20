9c6b6274d0d3dfb85e82390af03538b1b3b0b0d8
fullerak <fullerak@go.stockton.edu>
2023-04-20 15:30:22 -0400

    starting plotter

---
+++ ProbAndStats2/src/PSS.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class PSS {
+	
+	private int x;
+	private int y;
+
+	PSS(int xVal, int yVal) {
+		
+		x = xVal;
+		y = yVal;
+		
+	}
+	
+	public static void data(int min, int max, int range) {
+		
+		ArrayList<PSS> plot = new ArrayList<PSS>();
+		
+		int x;
+		int y;
+		
+		for(int i = min; i <= max; i = i + range) {
+			
+			if (i == min) {
+				
+			}
+		}
+		
+	}
+	
+	
+	
+}
