aaf16273abc7a47ebaf9864df09a108d99bec044
fullerak <fullerak@go.stockton.edu>
2023-04-20 15:49:03 -0400

    finishing the plotter

--- ProbAndStats2/src/PSS.java
+++ ProbAndStats2/src/PSS.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class PSS {
@@ -12,20 +15,50 @@ public class PSS {
 		
 	}
 	
-	public static void data(int min, int max, int range) {
+	public int getX() {
+		
+		return x;
+	}
+	
+	public int getY() {
+		
+		return y;
+	}
+	
+	public static void data(int min, int max, int range) throws FileNotFoundException{
 		
 		ArrayList<PSS> plot = new ArrayList<PSS>();
 		
-		int x;
-		int y;
+		int x = 0;
+		int y = 0;
 		
 		for(int i = min; i <= max; i = i + range) {
 			
 			if (i == min) {
 				
+				x = min;
+				
 			}
+			else {
+				
+				x += range;
+				
+			}
+			
+			y = 2 * x + 3;
+			
+			plot.add(new PSS(x, y));
 		}
 		
+		File csv = new File("Graph.csv");
+		PrintWriter out = new PrintWriter(csv);
+		
+		for (PSS graph :plot) {
+			
+			out.printf("%d, %d\n", graph.getX(), graph.getY());
+		}
+		
+		out.close();
 	}
 	
 	
