c57f2142f5fc260c54167a8ce7d43f90b880f55a
fullerak <fullerak@go.stockton.edu>
2023-04-23 12:26:28 -0400

    making some changes

---
+++ ProbAndStats2/src/Salter.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+
+public class Salter {
+
+	public static void salt(int range) {
+		
+		String file = "C:\\Users\\Kaden\\git\\ProbAndStatsProject2\\ProbAndStats2";
+		BufferedReader reader = null;
+		String line;
+		int temp;
+		
+		
+		
+		
+	}
+	
+	
+}
