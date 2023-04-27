f9411e5699ba9262595eb99ecbe1582d5add5aad
fullerak <fullerak@go.stockton.edu>
2023-04-27 10:50:12 -0400

    comitting the jfree

--- ProbAndStats2/src/LibraryGraphing.java
+++ ProbAndStats2/src/LibraryGraphing.java
@@ -1,4 +1,20 @@
+import java.awt.Color; 
+import java.awt.BasicStroke; 
+
+import org.jfree.chart.ChartPanel; 
+import org.jfree.chart.JFreeChart; 
+import org.jfree.data.xy.XYDataset; 
+import org.jfree.data.xy.XYSeries; 
+import org.jfree.ui.ApplicationFrame; 
+import org.jfree.ui.RefineryUtilities; 
+import org.jfree.chart.plot.XYPlot; 
+import org.jfree.chart.ChartFactory; 
+import org.jfree.chart.plot.PlotOrientation; 
+import org.jfree.data.xy.XYSeriesCollection; 
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 
 public class LibraryGraphing {
 //
+	
+	
 }
